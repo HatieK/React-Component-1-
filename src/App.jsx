13f767dfd90410e4df2c3ec67c738b882880f589
hainguyendev <n.thanhhai2306@gmail.com>
2024-05-25 19:36:09 +0700

    done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import MainComponent from "./Exercise-Component/MainComponent";
+
+function App() {
+  return (
+    <>
+      {/* Exercise 1 */}
+      <MainComponent />
+    </>
+  );
+}
+
+export default App;
