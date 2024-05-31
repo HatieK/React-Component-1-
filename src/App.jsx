8cc4d0b990a072025475552730eb706d43c0dc14
hainguyendev <n.thanhhai2306@gmail.com>
2024-05-31 09:43:20 +0700

    exercise 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import MainComponent from "./Exercise-Component/MainComponent";
+import Glasses from "./Exercise-Glasses/Glasses";
 
 function App() {
   return (
     <>
       {/* Exercise 1 */}
-      <MainComponent />
+      {/* <MainComponent /> */}
+
+      {/* Exercise 2 */}
+      <Glasses />
     </>
   );
 }
